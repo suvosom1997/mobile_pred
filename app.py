d7aaa5d2136eed7890f4fbd15673e557e8211b00
suvosom1997 <suvosom1997@gmail.com>
2022-09-09 16:20:19 +0530

    Code updated

--- app.py
+++ app.py
@@ -43,10 +43,19 @@ def predict():
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The price prediction is {}".format(output))
+    if output == 0:
+        return render_template("home.html",prediction_text="REGULAR, with a price range of Rs 5000 and Rs 8000")
+    elif output == 1:
+        return render_template("home.html",prediction_text="AFFORDABLE, with a price range of Rs 8000 and Rs 12000")
+    elif output == 2:
+        return render_template("home.html",prediction_text="COSTLY, with a price range of Rs 12000 and Rs 15000")
+    elif output == 3:
+        return render_template("home.html",prediction_text="VERY COSTLY, with a price range greater than Rs 15000")
+
+    
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True )
 
 
 
