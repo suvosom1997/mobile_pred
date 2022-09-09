7bf07a520cbc5cc4e26a083bc447cb296ee52793
suvosom1997 <suvosom1997@gmail.com>
2022-09-09 14:14:59 +0530

    Template updated

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from crypt import methods
+import json
+import flask
 import pickle
-from pyexpat import model
 from flask import Flask, request, app, jsonify, url_for, render_template
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
-
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
@@ -19,17 +18,27 @@ from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import classification_report,confusion_matrix
 
 app = Flask(__name__)
-p_model= pickle.load(open('knn_model.pkl','rb'))
+model= pickle.load(open('model.pkl','rb'))
+scalar= pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 
-def home:
+def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods = ['POST'])
 
-def predict api():
+def predict_api():
     data = request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data= scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output= model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
+
 
 
